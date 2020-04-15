bf729572e39d743b8a69567339b0c4538c50f187
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-04-15 01:26:23 -0300

    Estilos Globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,9 +3,8 @@ import {Title} from './styles.js';
 
 export default function Main() {
   return (
-  <Title error={false}>
-    Main
-    <small>Texto menor</small>
+  <Title>
+  Hello Word!
     </Title>
   );
 }
