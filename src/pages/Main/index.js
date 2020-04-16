49a6b382d972ecd1edb1cf735b0193ead571b7bc
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-04-16 00:43:05 -0300

    Estilizando pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import {Title} from './styles.js';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container , Form, SubmitButton} from './styles.js';
 
 export default function Main() {
   return (
-  <Title>
-  Hello Word!
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+          Repositórios
+      </h1>
+
+      <Form onSubmit={ () => {}}>
+        <input
+        type="text"
+        placeholder="Adiconar repositorio"/>
+
+        <SubmitButton disable>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 
